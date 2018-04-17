9f457ea479866daf70102508d003d58f10067165
Martin <martinjonsson01@gmail.com>
2018-04-17 12:48:40 +0200

    K I R B [B] Y

--- Godisautomat/Pages/BuyPage.xaml.cs
+++ Godisautomat/Pages/BuyPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Godisautomat.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace Godisautomat.Pages
     /// <summary>
     /// Interaction logic for BuyPage.xaml
     /// </summary>
-    public partial class BuyPage : BasePage
+    public partial class BuyPage : BasePage<BuyPageViewModel>
     {
         public BuyPage()
         {
---
+++ Godisautomat/ViewModels/BuyPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Godisautomat.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Godisautomat.ViewModels
+{
+    public class BuyPageViewModel : BaseViewModel
+    {
+    }
+}
